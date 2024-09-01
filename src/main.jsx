b6d45cccd49c754313abf66dd22bf3e961ceae85
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-09-01 22:37:16 +0530

    first-commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+ import React from 'react'
+ import ReactDOM from 'react-dom/client'
+ import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
